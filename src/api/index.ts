bef874ecfd2ad84693eb5b36354ddb05ea48513e
teohen <teodorohenriquecarvalho@gmail.com>
2020-11-22 19:07:38 -0300

    fix(prettier): prettier config

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,16 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const host: string =  process.env.NEXT_PUBLIC_ENV === 'development' ? process.env.NEXT_PUBLIC_DEV_HOST : 'producao'
-const port: string = process.env.NEXT_PUBLIC_ENV === 'development' ? process.env.NEXT_PUBLIC_DEV_PORT : '1'
+const host: string =
+  process.env.NEXT_PUBLIC_ENV === 'development'
+    ? process.env.NEXT_PUBLIC_DEV_HOST
+    : 'producao'
 
+const port: string =
+  process.env.NEXT_PUBLIC_ENV === 'development'
+    ? process.env.NEXT_PUBLIC_DEV_PORT
+    : '1'
 
 const api = axios.create({
-    baseURL: `http://${host}:${port}`
-});
-
-
-export default api;
+  baseURL: `http://${host}:${port}`
+})
+export default api
