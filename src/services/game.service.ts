3a2c67df74d9069406d52a6c29b84862c46f6c0d
teohen <teodorohenriquecarvalho@gmail.com>
2020-11-22 19:09:24 -0300

    feat(get - game): created the service that requests one game on the backen d

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,24 +1,36 @@
-import api from "../api"
+import api from '../api'
 
-interface Game{
+export interface Game {
   id: number
   code: string
   nPlayers: number
 }
 
 export const GameService = {
-  async create(numPlayers:number): Promise<Game>  {
+  async create(numPlayers: number): Promise<Game> {
     try {
       const { data } = await api.post('game', {
         nPlayers: numPlayers
-        })
+      })
 
-        return <Game>{
-          id: data.id,
-          code: data.code,
-          nPlayers: data.nPlyers
-        }
-    }catch(err){
+      return <Game>{
+        id: data.id,
+        code: data.code,
+        nPlayers: data.nPlyers
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  async get(id: string): Promise<Game> {
+    try {
+      const { data } = await api.get(`game/${id}`)
+      return <Game>{
+        id: data.id,
+        code: data.code,
+        nPlayers: data.nPlyers
+      }
+    } catch (err) {
       console.log(err)
     }
   }
